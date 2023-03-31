4f2aecd7f58b4b24a31c27b3081e7f0dc0a59130
vedant <ursvedantyetekar@gmail.com>
2023-04-01 00:08:58 +0530

    done setting up routing on frontend and built an ui

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import AddBook from "./components/AddBook";
+import Books from "./components/Book/Books";
+import About from "./components/About";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/add" element={<AddBook />} exact />
+          <Route path="/books" element={<Books />} exact />
+          <Route path="/about" element={<About />} exact />
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default App;
