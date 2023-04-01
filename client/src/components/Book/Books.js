d192ae63a7baf6f820d9d34f37059bce8d8b654d
vedant <ursvedantyetekar@gmail.com>
2023-04-02 00:30:28 +0530

    done get api integration

--- client/src/components/Book/Books.js
+++ client/src/components/Book/Books.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Book.css";
+import Book from "./Book";
+
+const url = "http://localhost:5000/books";
+const fetchHandler = async () => {
+  return await axios.get(url).then((res) => res.data);
+};
 
 const Books = () => {
-  return <div>Books</div>;
+  const [books, setBooks] = useState();
+  useEffect(() => {
+    fetchHandler().then((data) => setBooks(data.books));
+  }, []);
+  console.log(books);
+  return (
+    <div>
+      <ul>
+        {books &&
+          books.map((book, i) => {
+            return (
+              <li key={i}>
+                <Book book={book} />
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
 };
 
 export default Books;
